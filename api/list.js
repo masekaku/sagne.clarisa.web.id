0344cb2dc8be4cf59a9390c648444fa1ea6b481d
masekaku <aguslistiyanto27@gmail.com>
2025-11-19 22:57:43 +0700

    Create list.js

---
+++ api/list.js
@@ -0,0 +1,7 @@
+const videos = require('../videos.json');
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
+  return res.json(videos);
+}
