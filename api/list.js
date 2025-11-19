ff5e4e90c81b8756f733bc9e8fd50a70d7ec9316
masekaku <aguslistiyanto27@gmail.com>
2025-11-19 23:08:55 +0700

    Update list.js

--- api/list.js
+++ api/list.js
@@ -1,7 +1,7 @@
-const videos = require('../videos.json');
+import videosData from '../videos.json' assert { type: 'json' };
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
-  return res.json(videos);
+  return res.json(videosData);
 }
